73580393e233a47e373153f4ab97949e49a9e593
Manjeet <manjeetkamboj394@gmail.com>
2022-08-04 22:39:10 +0530

    homepage update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,13 @@ function Home({isAuth,userName}) {
         <div className="home-header">
         <h1>Hello {userName}</h1>
         <p>welcome to connect.</p>
+        <blockquote>"In every community there is work to be done. In every nation, there are wounds to heal. In every heart there is power to do it."</blockquote>
+        </div>
+        <div className='scroll-one'>
+          <h2>What is Connect?</h2>
+          <p>Connect is a platform where you can make a difference in the lives of people around you.</p>
+          <h2>Our vision</h2>
+          <p>Our vision is to make sure that no one sleeps hungry. We aim at redistrubution of resources from the ones who have them in access to the ones who are in need for the same.</p>
         </div>
     </div>
   )
